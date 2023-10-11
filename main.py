37366494b7220021e4735a668c1287b20497237c
wyochris <chrislardnercl@gmail.com>
2023-10-11 15:56:34 -0400

    implement main loop

---
+++ main.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+import os
+import discord
+import random
+
+
+load_dotenv()
+discord_token = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+client = discord.Client(intents=discord.Intents.all())
+
+# set up bot
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Game(name='!ethan help!'))
+
+# bot main loop
+@client.event
+async def on_message(message):
+    
+    # Ignore messages sent by the bot
+    if message.author == client.user:
+        return
+    
+
+def run():
+    try:
+        client.run(discord_token)
+    except Exception as e:
+        # let's me know if client.run() doesn't work
+        print(f'Chris broke the bot: {e}')
+
+run()
